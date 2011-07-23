5e26957de796614098b7887fbf2bf98641112149
Peter McEvoy <macint.94@gmail.com>
2011-07-23 23:16:25 +0200

    adasd script

--- start_faye.sh
+++ start_faye.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/sites/pollster.mlinux.ath.cx/pollster
-screen -A -m -d -S faye rackup faye.rb -s thin -E production
+screen -A -m -d -S faye rackup faye.ru -s thin -E production
