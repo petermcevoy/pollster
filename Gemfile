08bb7bc4cc79f6f30271833876d2711a133fdbb8
Peter McEvoy <macint.94@gmail.com>
2013-04-07 14:34:08 +0200

    changed front page

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ group :development do
 end
 
 group :production do
-	gem 'mysql2'
+	#gem 'mysql2'
 end
 
 # Asset template engines
--- config/initializers/start_faye.rb
+++ config/initializers/start_faye.rb
@@ -1,3 +1,3 @@
 Thread.new do
-  system("rackup faye.ru -s thin -E production")
+  #system("rackup faye.ru -s thin -E production")
 end
